7cdf7ca2285a15dc85d8f3385a355063d7e71c78
Trausti Thor Johannsson <traustitj@mac.com>
2021-08-21 22:45:51 +0000

    adding years as variables

--- rsk.go
+++ rsk.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"./socialid"
+	"runtime"
 )
 
 func generateForYear(year int, c chan []string) {
@@ -11,17 +12,21 @@ func generateForYear(year int, c chan []string) {
 }
 
 func main() {
+	numcpu := runtime.NumCPU()
+	from := 1700
+	to := 2018
 	c := make(chan []string)
 
-	for i := 1900; i < 2018; i++ {
+	for i := from; i < to; i++ {
 		go generateForYear(i, c)
 	}
 	total := 0
-	for i := 1900; i < 2018; i++ {
+	for i := from; i < to; i++ {
 		x := <-c
 		fmt.Printf("%d ids for year %d\n", len(x), i)
 		total += len(x)
 	}
 
 	fmt.Println("Totally generated numbers : ", total)
+	fmt.Println("Number of cpu's : ", numcpu)
 }
